a5c118cbc4503a1569de8e4cd3199f7c97e432a5
@au340161 <201304090@post.au.dk>
2024-03-19 18:04:04 +0100

    adjust tools in AboutMe

--- src/pages/home/AboutMe.jsx
+++ src/pages/home/AboutMe.jsx
@@ -32,22 +32,22 @@ function AboutMe() {
                 <div ref={ref2} className={`flex flex-col items-center lg:items-end lg:w-1/2 ${transitionClasses2}`}>
                     <div>
                         <div className='flex justify-center items-center rounded-full bg-gray-800 relative mx-auto w-64 h-64 xsm:w-80 xsm:h-80 sm:h-96 sm:w-96'>
-                            <RiJavascriptFill className='absolute                   top-0                       size-12                                         xsm:size-16                                 sm:size-20' />
-                            <FaReact className='absolute                            top-7 right-9               size-10     xsm:top-9 xsm:right-10              xsm:size-14     sm:top-10 sm:right-14       sm:size-16 ' />
-                            <SiNextdotjs className='absolute                        top-7 left-9                size-10     xsm:top-9 xsm:left-10               xsm:size-14     sm:top-10 sm:left-14        sm:size-16' />
-                            <SiTailwindcss className='absolute                      top-16                      size-12     xsm:top-[5.5rem]                    xsm:size-14     sm:top-28                   sm:size-16' />
-                            <FaCss3Alt className='absolute                          top-[5.7rem] right-12       size-10     xsm:top-28 xsm:right-14             xsm:size-14     sm:top-32                   sm:size-16' />
-                            <FaGitAlt className='absolute                           top-[5.5rem] left-12        size-10     xsm:top-[6.8rem] xsm:left-14        xsm:size-14     sm:top-[7.9rem]             sm:size-16' />
-                            <FaPython className='absolute                           right-0                     size-8                                          xsm:size-10                                 sm:size-12' />
-                            <HiCommandLine className='absolute                      left-0                      size-8                                          xsm:size-10                                 sm:size-12' />
-                            <FaHtml5 className='absolute                            bottom-20 left-20           size-8      xsm:bottom-24 xsm:left-24           xsm:size-10     sm:bottom-28 sm:left-28     sm:size-12' />
-                            <TbSql className='absolute                              bottom-[4.8rem] right-20    size-12     xsm:bottom-[5.8rem] xsm:right-24    xsm:size-14     sm:bottom-28 sm:right-28    sm:size-16' />
-                            <BiLogoGraphql className='absolute                      bottom-16 right-4           size-8      xsm:bottom-20 xsm:right-6           xsm:size-10                                 sm:size-12' />
-                            <BiLogoTypescript className='absolute                   bottom-16 left-4            size-8      xsm:bottom-20 xsm:left-6            xsm:size-10                                 sm:size-12' />
-                            <FaDocker className='absolute                           bottom-12                   size-6      xsm:bottom-14                       xsm:size-8      sm:bottom-16                sm:size-10' />
-                            <SiGooglecloud className='absolute                      bottom-4 right-2/3          size-6      xsm:bottom-6                        xsm:size-8                                  sm:size-10' />
-                            <SiDjango className='absolute                           bottom-4 left-2/3           size-6      xsm:bottom-6                        xsm:size-8                                  sm:size-10' />
-                            <FaJava className='absolute                             bottom-0                    size-6                                          xsm:size-8                                  sm:size-10' />
+                            <RiJavascriptFill className='absolute                   -top-1                       size-12    xsm:-top-[6px]                      xsm:size-16     sm:-top-2                           sm:size-20' />
+                            <FaReact className='absolute                            top-7 right-9               size-10     xsm:top-9 xsm:right-10              xsm:size-14     sm:top-10 sm:right-14               sm:size-16 ' />
+                            <SiNextdotjs className='absolute                        top-7 left-9                size-10     xsm:top-9 xsm:left-10               xsm:size-14     sm:top-10 sm:left-14                sm:size-16' />
+                            <SiTailwindcss className='absolute                      top-16                      size-12     xsm:top-[5rem]                      xsm:size-14     sm:top-24                           sm:size-16' />
+                            <TbSql className='absolute                              top-[5.2rem] right-12       size-12     xsm:top-[6.5rem] xsm:right-16       xsm:size-16     sm:top-[7.4rem] sm:right-[4.4rem]   sm:size-20' />
+                            <BiLogoTypescript className='absolute                   top-[5.5rem] left-12        size-10     xsm:top-[6.8rem] xsm:left-16        xsm:size-14     sm:top-[7.9rem]                     sm:size-16' />
+                            <FaPython className='absolute                           right-0                     size-8                                          xsm:size-10                                         sm:size-12' />
+                            <FaGitAlt className='absolute                           left-0                      size-8                                          xsm:size-10                                         sm:size-12' />
+                            <FaHtml5 className='absolute                            bottom-20 left-20           size-8      xsm:bottom-24 xsm:left-24           xsm:size-10     sm:bottom-28 sm:left-28             sm:size-12' />
+                            <FaCss3Alt className='absolute                          bottom-20 right-20          size-8      xsm:bottom-24 xsm:right-24          xsm:size-10     sm:bottom-28 sm:right-28            sm:size-12' />
+                            <BiLogoGraphql className='absolute                      bottom-16 right-4           size-8      xsm:bottom-20 xsm:right-6           xsm:size-10                                         sm:size-12' />
+                            <HiCommandLine className='absolute                      bottom-16 left-4            size-8      xsm:bottom-20 xsm:left-6            xsm:size-10                                         sm:size-12' />
+                            <FaDocker className='absolute                           bottom-12                   size-6      xsm:bottom-14                       xsm:size-8      sm:bottom-16                        sm:size-10' />
+                            <SiGooglecloud className='absolute                      bottom-4 right-2/3          size-6      xsm:bottom-6                        xsm:size-8                                          sm:size-10' />
+                            <SiDjango className='absolute                           bottom-4 left-2/3           size-6      xsm:bottom-6                        xsm:size-8                                          sm:size-10' />
+                            <FaJava className='absolute                             bottom-0                    size-6                                          xsm:size-8                                          sm:size-10' />
                         </div>
                         <p className='mt-4 text-center'>Some tools I am familiar with</p>
                     </div>
