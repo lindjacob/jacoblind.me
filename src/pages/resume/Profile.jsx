ac9c0dc168dfbbf5a4c74716238f55bc913a5c6d
@au340161 <jacob.lind2@gmail.com>
2024-06-06 10:59:24 +0200

    add eslint

--- src/pages/resume/Profile.jsx
+++ src/pages/resume/Profile.jsx
@@ -1,15 +1,15 @@
-import Container from './components/Container';
-import IconBox from './components/IconBox';
-import Location from './components/Location';
-import { PiUserList } from 'react-icons/pi';
+import Container from "./components/Container";
+import IconBox from "./components/IconBox";
+import Location from "./components/Location";
+import { PiUserList } from "react-icons/pi";
 
 export default function Profile({ data }) {
-    return (
-        <Container className='w-full md:w-2/3 pb-2'>
-            <IconBox IconComponent={PiUserList} />
-            <h2>Professional profile</h2>
-            <p>{data.profile.professional}</p>
-            <Location locationText={data.location} />
-        </Container>
-    )
+  return (
+    <Container className="w-full md:w-2/3 pb-2">
+      <IconBox IconComponent={PiUserList} />
+      <h2>Professional profile</h2>
+      <p>{data.profile.professional}</p>
+      <Location locationText={data.location} />
+    </Container>
+  );
 }
